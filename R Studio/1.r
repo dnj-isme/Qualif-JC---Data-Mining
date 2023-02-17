40b1f9d1b925362a5518e7d2cf939d83e32beca5
dnj-isme <dnj.isme@gmail.com>
2023-02-17 18:15:58 +0700

    RapidMiner

--- R Studio/1.r
+++ R Studio/1.r
@@ -8,17 +8,18 @@ summary$Total.Score <- round(
   (summary$Mid.Exam.x * summary$Mid.Exam.y) +
   (summary$Final.Exam.x * summary$Final.Exam.y))
 
-for (i in 1:length(summary$Total.Score)) {
-  input = summary$Total.Score[i]
-  if(input >= 90) summary$Grade[i] <- "A"
-  else if (input >= 85) summary$Grade[i] <- "A-"
-  else if (input >= 80) summary$Grade[i] <- "B+"
-  else if (input >= 75) summary$Grade[i] <- "B"
-  else if (input >= 70) summary$Grade[i] <- "B-"
-  else if (input >= 65) summary$Grade[i] <- "C"
-  else if (input >= 50) summary$Grade[i] <- "D"
-  else summary$Grade[i] <- "E"
+determine <- function(input) {
+  if(input >= 90) return("A")
+  else if (input >= 85) return("A-")
+  else if (input >= 80) return("B+")
+  else if (input >= 75) return("B")
+  else if (input >= 70) return("B-")
+  else if (input >= 65) return("C")
+  else if (input >= 50) return("D")
+  else return("E")
 }
+
+summary$Grade = sapply(summary$Total.Score, determine)
 
 df <- data.frame(Student.Name = summary$Student.Name, 
                  Course.Code = summary$Course.Code,
